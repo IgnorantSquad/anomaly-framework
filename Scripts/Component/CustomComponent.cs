507fd0efee85ab6422e0f0cfa883342435153391
IDstorage <idstorage1892@gmail.com>
2022-04-28 23:32:36 +0900

    CustomComponent Refactoring

--- Scripts/Component/CustomComponent.cs
+++ Scripts/Component/CustomComponent.cs
@@ -2,15 +2,9 @@ namespace Anomaly
 {
     public abstract class CustomComponent
     {
-        protected CustomBehaviour target;
-
-        public virtual void Initialize(CustomBehaviour target)
+        [System.Serializable]
+        public abstract class BaseData
         {
-            this.target = target;
         }
-
-#if UNITY_EDITOR
-        public abstract void OnInspectorGUI(UnityEditor.Editor editor, UnityEditor.SerializedProperty target);
-#endif
     }
 }
