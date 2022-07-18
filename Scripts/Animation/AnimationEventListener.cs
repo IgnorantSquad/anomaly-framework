b8f55f6fae5fd017c5924a46ec659d24c2aa938a
IDstorage <idstorage1892@gmail.com>
2022-07-19 03:41:18 +0900

    Update AnimationEventListener

--- Scripts/Animation/AnimationEventListener.cs
+++ Scripts/Animation/AnimationEventListener.cs
@@ -8,19 +8,17 @@ namespace Anomaly
     [System.Serializable]
     public class AnimationEventCallback : UnityEngine.Events.UnityEvent<AnimationEvent> { }
 
-    public class AnimationEventListener : CustomObject
+    public class AnimationEventListener : CustomBehaviour
     {
-        // public AnimatorComponent.Data data;
+        public SerializableDictionary<AnimationEventCallback> animationEvents = new SerializableDictionary<AnimationEventCallback>();
 
-        // public SerializableDictionary<AnimationEventCallback> animationEvents = new SerializableDictionary<AnimationEventCallback>();
+        public void OnEvent(AnimationEvent param)
+        {
+            string function = param.stringParameter.Contains("|") ? param.stringParameter.Split('|')[0] : param.stringParameter;
+            if (!animationEvents.Container.ContainsKey(function)) return;
 
-        // public void OnEvent(AnimationEvent param)
-        // {
-        //     string function = param.stringParameter.Contains("|") ? param.stringParameter.Split('|')[0] : param.stringParameter;
-        //     if (!animationEvents.Container.ContainsKey(function)) return;
-
-        //     animationEvents.Container[function]?.Invoke(param);
-        // }
+            animationEvents.Container[function]?.Invoke(param);
+        }
     }
 }
 
