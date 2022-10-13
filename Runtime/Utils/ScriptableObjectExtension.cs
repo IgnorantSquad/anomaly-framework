0ca939508812e3afa95a13f3a1f49d118725478a
IDstorage <idstorage1892@gmail.com>
2022-10-13 11:09:46 +0900

    Remove useless listener & Add ScriptableObjectExtension (clone)

---
+++ Runtime/Utils/ScriptableObjectExtension.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScriptableObjectExtension
+{
+    public static T Clone<T>(this T so) where T : ScriptableObject
+    {
+        if (so == null)
+        {
+            return ScriptableObject.CreateInstance<T>();
+        }
+
+        T instance = Object.Instantiate(so);
+        instance.name = so.name;
+        return instance;
+    }
+}
